9657a2957000cfc7e8d90831f4db6cf67fb3f0b3
Ronald Prato <37622085+Ronald-Prato@users.noreply.github.com>
2019-08-05 17:37:46 -0500

    Add files via upload

---
+++ build/precache-manifest.d91f01a838b623ecb04dbeb62963e523.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3b0fa13abfd9cc56d77c3676578b3c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37b689942bfb266bbc05",
+    "url": "/static/css/2.c6d260ee.chunk.css"
+  },
+  {
+    "revision": "afcd8d9399b1c49a2c07",
+    "url": "/static/css/main.0d45fb32.chunk.css"
+  },
+  {
+    "revision": "37b689942bfb266bbc05",
+    "url": "/static/js/2.1b7e2490.chunk.js"
+  },
+  {
+    "revision": "afcd8d9399b1c49a2c07",
+    "url": "/static/js/main.0ffe0df9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e30f957f3a0b4927ca9d5cf8b0d87977",
+    "url": "/static/media/Ajax.e30f957f.svg"
+  },
+  {
+    "revision": "07b3a1b190bc0da8b504b67b5697df2c",
+    "url": "/static/media/Amazon.07b3a1b1.svg"
+  },
+  {
+    "revision": "792b0e25536a98dbb4464f9ae05c5b02",
+    "url": "/static/media/Azure.792b0e25.svg"
+  },
+  {
+    "revision": "931151fbae2941875a30e368c748dda6",
+    "url": "/static/media/Cloudera.931151fb.svg"
+  },
+  {
+    "revision": "600d33b0bc52d544da8c5ef2f08d518c",
+    "url": "/static/media/Lambda.600d33b0.svg"
+  },
+  {
+    "revision": "986372d697d1314024e8df37a8884212",
+    "url": "/static/media/LogoJustReact.986372d6.svg"
+  },
+  {
+    "revision": "eb3c72f87cd72f58ad97cefa0d291d53",
+    "url": "/static/media/LogoWoombat.eb3c72f8.svg"
+  },
+  {
+    "revision": "0548796f73950e64452760453be1d078",
+    "url": "/static/media/People-asking.0548796f.svg"
+  },
+  {
+    "revision": "2d0b5a3babde3aa3ff556a5211cd7297",
+    "url": "/static/media/User-icon-girl.2d0b5a3b.svg"
+  },
+  {
+    "revision": "7f7dd2461e8d534ec8d4a01eef0e47e6",
+    "url": "/static/media/User-icon.7f7dd246.svg"
+  },
+  {
+    "revision": "517cd79b7b8545fcd8bfe08bac64dd0a",
+    "url": "/static/media/bg.517cd79b.svg"
+  },
+  {
+    "revision": "252356ac675de6049c794ca3ccad4f92",
+    "url": "/static/media/pika.252356ac.mp3"
+  }
+]);
